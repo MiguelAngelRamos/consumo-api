3b95082e744d11eb7ba89f0fb8786ed1c53f5630
Miguel Angel Ramos <return.infox@gmail.com>
2024-04-12 10:55:25 -0400

    tarjetas de bootstrap

--- js/api-rest.js
+++ js/api-rest.js
@@ -5,7 +5,7 @@ const getCharacters = async () => {
   try {
     const response = await fetch("https://rickandmortyapi.com/api/character");
     const data = await response.json();
-   
+ 
     container.appendChild(generationRow(data.results));
   } catch (error) {
     throw new Error(error);
@@ -31,23 +31,31 @@ const generateCardBootstrap = (character) => {
   const statusColor = character.status.toLowerCase() === 'alive' ? 'green': character.status.toLowerCase() === 'unknown'? 'gray': 'red';
 
   return `
-    <div class="col-3 card mt-5" style="width: 18rem;">
-    <img src=${character.image} class="mt-2 p-2 card-img-top" alt="imagen del personaje">
-    <div class="card-body">
-      <h5 class="card-title fw-bold">${character.name}</h5>
-      <p class="card-text">
-        <span class="status-indicator" style="background-color:${statusColor};"></span>
-        ${character.status} - ${character.species}
-      </p>
-     
-      <p class="card-text" style="margin-bottom: 0;">
-        Last known location:
-      </p>
+    <div class="card d-flex mt-5" style="max-width: 40rem;">
+      <div class="row g-0">
+        <div class="col-md-5">
+        <img src=${character.image} class="img-fluid rounded-start" alt="imagen del personaje">
+        </div>
 
-      <p class="card-text">
-      ${character.location.name}
-      </p>
-    </div>
+        <div class="col-md-7">
+        <div class="card-body">
+          <h5 class="card-title fw-bold">${character.name}</h5>
+          <p class="card-text">
+            <span class="status-indicator" style="background-color:${statusColor};"></span>
+            ${character.status} - ${character.species}
+          </p>
+        
+          <p class="card-text" style="margin-bottom: 0;">
+            Last known location:
+          </p>
+
+          <p class="card-text">
+          ${character.location.name}
+          </p>
+        </div>
+      </div> 
+      </div>
+      
     </div>
   `;
 };
